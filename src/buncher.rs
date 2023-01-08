5593062e61efb8c616a4b5aac1d04a80a9b17a6f
Miezhiko <Miezhiko@gmail.com>
2023-01-08 15:28:37 +0400

    better progress bar, aka indicatif
    Signed-off-by: Miezhiko <Miezhiko@gmail.com>

--- src/buncher.rs
+++ src/buncher.rs
@@ -7,6 +7,7 @@ use crate::{
 };
 
 use std::{
+  time::Duration,
   collections::{
     hash_map::Entry,
     HashMap
@@ -15,7 +16,7 @@ use std::{
 
 use sha3::{ Digest, Sha3_256 };
 
-use pbr::ProgressBar;
+use indicatif::{ ProgressBar, ProgressStyle };
 
 const EXTENSIONS: &str = "*.{jpg,jpeg,png,tiff,webp,mp4}";
 
@@ -23,6 +24,24 @@ pub async fn process(args: &mut Args) -> anyhow::Result<()> {
   let path = &args.directory;
   let mut seen_hashes = HashMap::new();
   let mut seen_hashes_videos = HashMap::new();
+
+  let pb = ProgressBar::new_spinner();
+  pb.enable_steady_tick(Duration::from_millis(120));
+  pb.set_style(
+    ProgressStyle::with_template("{spinner:.blue} {msg}")
+      .unwrap()
+      // For more spinners check out the cli-spinners project:
+      // https://github.com/sindresorhus/cli-spinners/blob/master/spinners.json
+      .tick_strings(&[
+        "▹▹▹▹▹",
+        "▸▹▹▹▹",
+        "▹▸▹▹▹",
+        "▹▹▸▹▹",
+        "▹▹▹▸▹",
+        "▹▹▹▹▸",
+        "▪▪▪▪▪",
+      ])
+  );
 
   let (target_directory, new_target) = if let Some(target_dir) = &args.output {
     if Path::new(&target_dir).exists() {
@@ -103,7 +122,7 @@ pub async fn process(args: &mut Args) -> anyhow::Result<()> {
   let mut img_paths: Vec<PathBuf>   = vec![];
   let mut video_paths: Vec<PathBuf> = vec![];
 
-  println!("collecting: file paths...");
+  pb.set_message("collecting: file paths...");
   let walker = globwalk::GlobWalkerBuilder::from_patterns(
       path, &[EXTENSIONS]
     ).max_depth(4)
@@ -170,10 +189,15 @@ pub async fn process(args: &mut Args) -> anyhow::Result<()> {
       }
     }
   }
+  pb.finish_with_message("Done");
 
   println!("processing videos");
-  let mut pb_videos = ProgressBar::new(video_paths.len() as u64);
-  pb_videos.format("[=>_]");
+  let pb_videos = ProgressBar::new(video_paths.len() as u64);
+  pb_videos.set_style(
+    ProgressStyle::with_template(
+        "{spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] ({pos}/{len}, ETA {eta})",
+    )
+    .unwrap());
   for file_path in video_paths.into_iter() {
     if let Err(why) = process_vid( path
                                  , file_path
@@ -181,14 +205,18 @@ pub async fn process(args: &mut Args) -> anyhow::Result<()> {
                                  , &target_directory
                                  ).await {
       println!("Error processing video: {why}");
+      pb_videos.inc(1);
     }
-    pb_videos.inc();
   }
   pb_videos.finish();
 
   println!("processing images");
-  let mut pb_images = ProgressBar::new(img_paths.len() as u64);
-  pb_images.format("[=>_]");
+  let pb_images = ProgressBar::new(img_paths.len() as u64);
+  pb_images.set_style(
+    ProgressStyle::with_template(
+        "{spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] ({pos}/{len}, ETA {eta})",
+    )
+    .unwrap());
   for file_path in img_paths.into_iter() {
     if let Err(why) = process_img( path
                                  , file_path
@@ -199,7 +227,7 @@ pub async fn process(args: &mut Args) -> anyhow::Result<()> {
                                  ).await {
       println!("Error processing image: {why}");
     }
-    pb_images.inc();
+    pb_images.inc(1);
   }
   pb_images.finish();
 
